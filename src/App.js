6c23999c3daee846f94f352685957075bdc93774
Julien <julien@marmelab.com>
2016-11-24 15:24:12 +0100

    Remove unused saga

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import createSagaMiddleware from 'redux-saga';
 import createLogger from 'redux-logger';
 import Navigation from './Navigation';
 import RootReducer from './app/reducers/Root';
-import rootSaga from './app/sagas/Root';
 
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(RootReducer, applyMiddleware(sagaMiddleware, createLogger()));
-sagaMiddleware.run(rootSaga);
+const store = createStore(RootReducer, applyMiddleware(createLogger()));
 
 const App = () => (
     <Provider store={store}>
--- src/app/sagas/Root.js
+++
@@ -1,9 +0,0 @@
-import {
-    watchPlaceCellAsync,
-} from './Game';
-
-export default function* () {
-    yield [
-        watchPlaceCellAsync(),
-    ];
-}
